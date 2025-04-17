1ef9df3d426821f21332e828dd8031fa6a43272a
Augusto <augustogugu493@.br>
2025-04-17 11:04:55 -0300

    calculadora

---
+++ calculadora.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+   int  numero1, numero2;
+
+    
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &numero1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d",  &numero2);
+     
+     
+    printf("Resultado da soma: %d\n", numero1 + numero2);
+    printf("Resultado da subtração: %d\n", numero1 - numero2);
+    printf("Resultado da multiplição: %d\n", numero1 * numero2);
+    printf("Resultado da divisão: %d\n", numero1 / numero2);
+    printf("Resultado do resto da divisão: %d\n", numero2 % numero2);
+     
+     
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
